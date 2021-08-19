e2dbeca9f23a17fde1397ebe33ca8ca33f39b664
arryan <arryan@umich.edu>
2021-08-19 17:05:11 -0400

    get percentage of cpu used

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -15,5 +15,42 @@ const cpuSpeed = cpus['0'].speed;
 // number of cores
 const numCores = cpus.length;
 
+const cpuAverage = () => {
+  // refresh the cpu data
+  const cpus = os.cpus();
 
-console.log(numCores);
+  let idleMs = 0;
+  let totalMs = 0;
+
+  // loop through each core
+  cpus.forEach(core => {
+    // loop through each property of the current core
+    for (type in core.times){
+      totalMs += core.times[type];
+    }
+    idleMs += core.times.idle;
+  });
+
+  return {
+    idle: idleMs / cpus.length,
+    total: totalMs / cpus.length
+  }
+}
+
+// current cpu load
+const getCpuLoad = () => {
+  const start = cpuAverage();
+  setTimeout(() => {
+    const end = cpuAverage();
+    const idleDifference = end.idle - start.idle;
+    const totalDifference = end.total - start.total;
+
+    // calculate the percentage of used cpu
+    const percentageCpu = 100 - Math.floor(100 * idleDifference / totalDifference)
+    console.log(percentageCpu);
+  }, 100);
+}
+
+setInterval(() => {
+  getCpuLoad();  
+}, 1000);
