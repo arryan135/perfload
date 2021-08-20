86ce31d08516c802cfcde528c431c07ecf875106
arryan <arryan@umich.edu>
2021-08-19 21:30:48 -0400

    clear performance data ticking when the socket disconnects

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -25,6 +25,10 @@ socket.on("connect", () => {
       socket.emit("perfData", allPerformanceData);
     });
   }, 1000);
+
+  socket.on("disconnect", () => {
+    clearInterval(perfDataInterval);
+  });
 });
 
 const performanceData = () => {
